cb22576ec34a8578a9d1210284c1f3d449df1ca9
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-15 00:14:54 +0600

    Create Script.js

---
+++ Animation/Script.js
@@ -0,0 +1,32 @@
+
+//function method 
+
+//function frame() {
+  //if (/* test for finished */) {
+    //clearInterval(id);
+  //} else {
+    /* code to change the element style */ 
+  //}
+//}
+
+
+
+
+
+
+
+function myMove() {
+  var elem = document.getElementById("animate");   
+  var pos = 0;
+  var id = setInterval(frame,1);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++; 
+      elem.style.top= pos + "px"; 
+    
+    elem.style.left= pos + "px"; 
+    }
+  }
+}
