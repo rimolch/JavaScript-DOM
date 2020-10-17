caa4c5f4d0f7d44e1058c9d9c8bcb0d3d9733b68
Rimol chakma <67545874+rimolch@users.noreply.github.com>
2020-10-17 08:24:07 +0600

    Create Script.js

---
+++ addEventListener/Bubbling & capturing/Script.js
@@ -0,0 +1,15 @@
+document.getElementById("myP1").addEventListener("click", function() {
+  alert("You clicked the white element!");
+},false);
+
+document.getElementById("myDiv1").addEventListener("click", function() {
+  alert("You clicked the orange element!");
+},false );
+
+document.getElementById("myP2").addEventListener("click", function() {
+  alert("You clicked the white element!");
+},false);
+
+document.getElementById("myDiv2").addEventListener("click", function() {
+  alert("You clicked the orange element!");
+},false);
